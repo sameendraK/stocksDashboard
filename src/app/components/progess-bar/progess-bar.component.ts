fc1673a49f002620bfd48a999b9b7b6d4a21c529
sameendraK <sameendrak@gmail.com>
2023-12-23 18:27:37 +0530

    feat(progress-bar): Fix styling of progress bar

--- src/app/components/progess-bar/progess-bar.component.ts
+++ src/app/components/progess-bar/progess-bar.component.ts
@@ -8,10 +8,29 @@ import { Component, Input } from '@angular/core';
 export class ProgessBarComponent {
 
   @Input()
+  item: any;
+  // @Input()
   high: number;
-  @Input()
   low: number;
-  @Input()
+  // @Input()
   ltp: number;
+
+  calculateProgress(item: any): number {
+    const high = item.high;
+    const low = item.low;
+    const ltp = item.ltp;
+  
+    // Check for valid data range
+    if (high === low) {
+      return 0; // To avoid division by zero and ensure no progress when high and low are the same
+    }
+  
+    // Calculate progress percentage
+    const progress = ((ltp - low) / (high - low)) * 100;
+  
+    // Ensure progress stays within the range of 0 to 100
+    return Math.min(100, Math.max(0, progress));
+  }
+  
 
 }
